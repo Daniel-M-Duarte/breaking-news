42709aff2e08b3b9b54a92d5dbec2ad9345d601c
Daniel-M-Duarte <danielmacield@gmail.com>
2024-04-28 10:11:36 -0300

    ref: add news route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./src/routes/userRoute.js";
 import authRouter from "./src/routes/authRoute.js";
+import newsRouter from "./src/routes/newsRoute.js";
 import connectDataBase from "./src/database/db.js";
 
 import dotenv from "dotenv";
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use("/user", router);
 app.use('/login', authRouter);
+app.use("/news", newsRouter);
 
 const port = process.env.PORT || 3000;
 
